ad296d9b99a2eb724dca7f818b2e8028cd7c1119
Cameron Bunce <charliesinfiniteride@gmail.com>
2022-12-24 23:08:56 -0500

    Added Serial UART 	modified:   main.py 	modified:   README.md

--- main.py
+++ main.py
@@ -5,12 +5,24 @@
 # Code largely based on MicroPython Reference code
 # http://docs.micropython.org/en/latest/esp32/quickref.html#onewire-driver
 
-import time, ds18x20, onewire
-from machine import Pin
+import time, ds18x20, onewire, ubinascii, json
+from machine import Pin, UART
+
 ow = onewire.OneWire(Pin(2))
 ds = ds18x20.DS18X20(ow)
+uart1 = UART(1, baudrate=115200, tx=17, rx=16)
+
 roms = ds.scan()
-ds.convert_temp()
-time.sleep_ms(750)
-for rom in roms:
-    print(ds.read_temp(rom))
+uart1.write("Ready reading and writing\n\r")
+print(roms)
+while True:
+    dict={}
+    time.sleep_ms(500)
+    for rom in roms:
+        ds.convert_temp()
+        temp = ds.read_temp(rom)
+        print(temp)
+        uart1.write(str(temp)+"\r\n")
+        #dict[ubinascii.hexlify(rom)]=temp
+    #uart1.write(json.dumps(dict)+"\r\n")
+
