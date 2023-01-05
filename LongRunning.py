b2eec45ab260c4e3961f941b8645aa8f561ea7da
Cameron Bunce <charliesinfiniteride@gmail.com>
2023-01-05 00:07:57 -0500

    progress

--- LongRunning.py
+++ LongRunning.py
@@ -15,93 +15,50 @@
 #   If 24hrs of data is stored in the file, send it to Edge Impulse
 #   Sleep for 30 minutes
 
+class DS18B20Reader:
+    def __init__(self, pin):
+        import onewire, ds18b20
+        from machine import Pin
+        # setup Dallas sensor 
+        self.ow = onewire.OneWire(Pin(pin))
+        self.dallassensor = ds18b20.DS18B20(self.ow)
+        self.Values = {}
+        self.initializeValues()
 
-# send_values
-# this function manages network connection 
-# first checking to see if the intended SSID is available
-# then attempting connect and send 'values' 
-# Currently values is expected to be a list, but this will have to change
-def send_values(values):
-    import secret, json, ubinascii, network, hashlib, hmac
-    HMAC_KEY = secret.hmac_key
-    API_KEY = secret.api_key
-    mac = ubinascii.hexlify(wlan.config('mac')).decode()
-    ssid_list = []
-
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    RawList = wlan.scan()
-    for one in RawList:
-        ssid_list.append(one[0].decode())
-    
-    if not secret.ssid in ssid_list:
-        return False
-    else:
-        if not wlan.isconnected():
-            wlan.connect(secret.ssid, secret.password)
-            while not wlan.is_connected():
-                pass
-        # empty signature (all zeros). HS256 gives 32 byte signature, and we encode in hex, so we need 64 characters here
-        emptySignature = ''.join(['0'] * 64)
-
-        data = {
-            "protected": {
-                "ver": "v1",
-                "alg": "HS256",
-                "iat": time.time() # epoch time, seconds since 1970
-            },
-            "signature": emptySignature,
-            "payload": {
-                "device_name": mac,
-                "device_type": "ESP32-DS18B20",
-                "interval_ms": 10,
-                "sensors": [
-                    { "name": "Temperature", "units": "Celsius" }
-                ],
-                "values": values
-            }
-        }
-        # encode data to json
-        encoded = json.dumps(data)
-        # create signature based on blank signature field
-        signature = hmac.new(bytes(HMAC_KEY, 'utf-8'), msg = encoded.encode('utf-8'), digestmod = hashlib.sha256).hexdigest()
-
-        # update signature field to sign the message
-        data['signature'] = signature
-        encoded = json.dumps(data)
-
-        # POST the file
-        res = requests.post(url='https://ingestion.edgeimpulse.com/api/training/data',
-                            data=encoded,
-                            headers={
-                                'Content-Type': 'application/json',
-                                'x-file-name': 'idle01',
-                                'x-api-key': API_KEY
-                            })
-        wlan.active(False)
-        if (res.status_code == 200):
-            return True
-        else:
-            return False
-
-
+    def getSensors(self):
+        return Values.keys()
 
 #   InitializeValues
-#   Check folder /values for earlier readings
-#   check attached sensors
-def initializeValues(dallassensor)
-
-
-import json, time, hmac, hashlib
-import ubinascii, network, ds18x20, onewire, secret
-import urequests as requests
-from machine import Pin, WDT
-
-# This is copy-pasta right now, 
-# I'll organize it as the data requires
-
-ow = onewire.OneWire(Pin(2))
-ds = ds18x20.DS18X20(ow)
+#   Check values.json for earlier readings
+#   Check attached sensors for new sensors
+    def initializeValues(self):
+        import os, json, ubinascii
+        roms = self.dallassensor.scan()
+        if 'values.json' not in os.listdir('/'):
+            # create the data structure and store a blank copy based on the available sensors
+            
+            for rom in roms:
+                self.Values[ubinascii.hexlify(rom).decode()] = []
+            outfile = open('values.json', w)
+            outfile.write(json.dumps(Values))
+            outfile.close()
+        else:
+            # first read in the values stored
+            with open('values.json') as filein:
+                self.Values = json.load(filein)
+            # then see if we have any new sensors
+            keys = self.Values.keys()
+            for rom in roms:
+                strrom = ubinascii.hexlify(rom).decode()
+                if strrom not in keys:
+                    self.Values[strrom] = []
+    
+    def updateValues(self):
+        # read attached sensors and update our dictionary and file
+    
+    def getValues(
+        
+    )
 roms = ds.scan()
 ds.convert_temp()
 
@@ -123,62 +80,3 @@ while readings < 10:
     values.append(read_pass)
     time.sleep(10)
     readings+=1
-
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-if not wlan.isconnected():
-    wlan.connect(secret.ssid, secret.password)
-    while not wlan.isconnected():
-        pass
-
-
-HMAC_KEY = secret.hmac_key
-API_KEY = secret.api_key
-mac = ubinascii.hexlify(wlan.config('mac')).decode()
-
-# empty signature (all zeros). HS256 gives 32 byte signature, and we encode in hex, so we need 64 characters here
-emptySignature = ''.join(['0'] * 64)
-
-data = {
-    "protected": {
-        "ver": "v1",
-        "alg": "HS256",
-        "iat": time.time() # epoch time, seconds since 1970
-    },
-    "signature": emptySignature,
-    "payload": {
-        "device_name": mac,
-        "device_type": "ESP32-DS18B20",
-        "interval_ms": 10,
-        "sensors": [
-            { "name": "Temperature", "units": "Celsius" }
-        ],
-        "values": values
-    }
-}
-
-
-# encode in JSON
-encoded = json.dumps(data)
-
-
-# sign message
-signature = hmac.new(bytes(HMAC_KEY, 'utf-8'), msg = encoded.encode('utf-8'), digestmod = hashlib.sha256).hexdigest()
-
-# set the signature again in the message, and encode again
-data['signature'] = signature
-encoded = json.dumps(data)
-
-# and upload the file
-
-res = requests.post(url='https://ingestion.edgeimpulse.com/api/training/data',
-                    data=encoded,
-                    headers={
-                        'Content-Type': 'application/json',
-                        'x-file-name': 'idle01',
-                        'x-api-key': API_KEY
-                    })
-if (res.status_code == 200):
-    print('Uploaded file to Edge Impulse', res.status_code, res.content)
-else:
-    print('Failed to upload file to Edge Impulse', res.status_code, res.content)
